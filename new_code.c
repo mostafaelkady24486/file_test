67ba73d5953cb0dfb7e5945118e4b61f2daa76b4
mostafaelkady24486 <elkadymostafa34@gmail.com>
2021-08-15 22:24:48 +0200

    file

--- new_code.c
+++ new_code.c
@@ -3,11 +3,16 @@
 ////////////
 //
 //
-
+/*hewy hadfa;gdj;*/
 void main (void)
 {
 	
 	int x = 1457;
 	printf("this is the new value ");
+	/*hewy hadfa;gdj;*/
+
+/*hewy hadfa;gdj;*/
+/*hewy hadfa;gdj;*/
+
 	scanf(&x);
 }
